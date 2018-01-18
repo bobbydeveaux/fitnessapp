91eec874a39f41f336fe4c931aaaf730933992b0
Bobby DeVeaux <me@bobbyjason.co.uk>
2018-01-18 20:23:01 +0000

    adding user variables and more options

--- index.php
+++ index.php
@@ -1,24 +1,35 @@
 <?php
 
+$user = [
+	'guid' => '1s1d1d1fwf2ff3g4v4g4',
+	'name' => 'Bobby',
+	'sname' => 'DeVeaux',
+	'age'   => 32,
+	'height' => 180,
+	'lifestyle' => 'lchf',
+	'activity'  => 'low',
+	'deficit'   => 'moderate',
+	'mobile'    => '+447584900848',
+];
+
 $twilio['account'] = getenv('TWILIO_ACCOUNT');
 $twilio['user'] = getenv('TWILIO_USER');
 $twilio['auth'] = getenv('TWILIO_AUTH');
 
 $mass     = $_GET['mass'] ? $_GET['mass'] : 0;
 $bodyfat  = $_GET['bf'] ? $_GET['bf'] : 0;
-$height   = $_GET['height'] ? $_GET['height'] : 0; 
-$lifesyle = "lchf";
-$activity = "low";
-$deficit  = "moderate";
+$user['lifestyle'] = $_GET['ls'] ? $_GET['ls'] : $user['lifestyle'];
+$height   = $user['height'];
+$lifestyle = $user['lifestyle'];
+$activity = $user['activity'];
+$deficit  = $user['deficit'];
 
 if ($mass == 0 || $bodyfat == 0 || $height == 0) {
 	print "Mass / bodyfat / height must be > 0";
 	exit;
 }
 
-putenv("SEND_MESSAGE=true");
 putenv("DEBUG=true");
-
 
 
 $calc['fatmass']  = round($mass*($bodyfat/100),2);
@@ -31,11 +42,29 @@ switch ($activity) {
 		$activityFactor = 1.2;
 }
 
+switch ($deficit) {
+	default;
+	case "moderate":
+		$deficitGoal = 0.75;
+}
+
 $calc['tdee']         = round($calc['bmr']*$activityFactor,2);
-$calc['calorie_goal'] = round($calc['tdee']*0.75,0);
+$calc['calorie_goal'] = round($calc['tdee']*$deficitGoal,0);
 $calc['protein_goal'] = round($mass*2, 2);
-$calc['carb_goal']    = 60;
 $calc['protein_cals'] = round($calc['protein_goal']*4, 2);
+
+switch ($lifestyle) {
+	default;
+	case "lchf":
+		$calc['carb_goal'] = 30;
+		break;
+	case "lfhc":
+		// @TODO needs to be a percentage or minus fat cals
+		// this may cause higher calorie than permitted, or create negative fat.
+		$calc['carb_goal'] = 180;
+		break;
+}
+
 $calc['carb_cals']    = round($calc['carb_goal']*4, 2);
 $calc['fat_cals']     = round($calc['calorie_goal'] - $calc['protein_cals'] - $calc['carb_cals'], 2);
 $calc['fat_goal']     = round($calc['fat_cals'] / 9, 2);
@@ -51,8 +80,6 @@ $calc['height']  = $height;
 $output = json_encode($calc);
 //print $output;
 
-$user['name'] = 'Bobby';
-$user['mobile'] = '+447584900848';
 
 $message = formatMessage($user, $calc);
 
@@ -70,6 +97,8 @@ function formatMessage($user, $calc) {
 	$message .= "Fat: " . $calc['bodyfat'] . "%\n";
 	$message .= "Calorie goal: " . $calc['calorie_goal'] . "kcal!\n";
 	$message .= "Protein goal: " . $calc['protein_goal'] . "g\n";
+	$message .= "Carb goal: " . $calc['carb_goal'] . "g\n";
+	$message .= "Fat goal: " . $calc['fat_goal'] . "g\n";
 	$message .= "BMI: " . $calc['bmi'] . "\n";
 	$message .= "Good luck fatty!";
 
